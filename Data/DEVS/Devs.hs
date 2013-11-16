671730ac718128db49114478092545e7d7a6bcc4
Markus Barenhoff <mbarenh@alios.org>
2013-11-16 15:15:50 +0100

    added license comment

--- Data/DEVS/Devs.hs
+++ Data/DEVS/Devs.hs
@@ -26,12 +26,32 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 -}
 
-module Data.DEVS.Simulation
-    ( module Data.DEVS.Simulation.Simulator
-    , module Data.DEVS.Simulation.Coordinator
-    ) where
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Data.DEVS.Devs (T, DEVS (..), CoupledModel (..)) where
+
+import Data.Binary
+import Data.Typeable (Typeable)
+import Data.Set (Set)
 
 
-import Data.DEVS.Simulation.Simulator
-import Data.DEVS.Simulation.Coordinator
+type T = Double
+
+class (Typeable m, Ord (X m), Binary (X m), Binary (Y m)) => DEVS m where
+    data S m :: *
+    type X m :: *
+    type Y m :: *
+
+    s0 :: m -> S m
+    lambda :: m -> S m -> Y m
+    delta_ext :: m -> S m -> T -> Set (X m) -> S m
+    delta_int :: m -> S m -> S m
+    delta_con :: m -> S m -> Set (X m) -> S m
+    ta :: m -> S m -> T
+ 
+class (DEVS m) => CoupledModel m where
+    data CX m :: *
+    data CY m :: *
+             
 
